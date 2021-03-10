f7b37bc6081f758d947de153294373d6f88cd222
PC_rajesh <rajeshPC@example.com>
2021-03-10 23:42:02 +0530

    added my google credentials

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     google: {
-        clientID: '297501645040-e56jt4u78qbfu7kh16uhughjlt08i3f0.apps.googleusercontent.com',
-        clientSecret: '4WaNtW-hNhHsca7SE-feq9TW'
+        clientID: '435677045413-56d1hl3t99g18brkndqhivjk5toelv6k.apps.googleusercontent.com',
+        clientSecret: 'WDBJQDxzbuRoiRixG9M5dAJ8'
     },
     mongodb:{
         dbURI:'mongodb+srv://rajesh:admin@cluster0.dzaoe.mongodb.net/employee?retryWrites=true&w=majority'
@@ -12,3 +12,10 @@ module.exports = {
 };
 
 // mongodb://localhost:27017/employees
+
+//on credentials http://localhost:3000
+
+// google: {
+//     clientID: '297501645040-e56jt4u78qbfu7kh16uhughjlt08i3f0.apps.googleusercontent.com',
+//     clientSecret: '4WaNtW-hNhHsca7SE-feq9TW'
+// },
